8a4f23ec9408f28466f05e53ee07a799b557e5d8
isaac cordova <tumbadoisaac5@gmail.com>
2024-11-04 14:47:24 -0600

    Add files via upload
    actualizacion para mostrar los productos en el index

--- index.php
+++ index.php
@@ -1,10 +1,30 @@
+<?php
+  $BDProductos = array( 
+    array(1, 2, 3, 4, 5, 6, 7),                                       //El ID
+    array("Blusa", "Lentes", "Playera", "Pantalón", "Vestido", "Playera Roja", "calcetines"), //La Descripción
+    array(100, 350, 200, 420, 250, 170, 60),                             //El Precio
+    array("Dama", "Caballero", "Caballero", "Dama", "Dama", "Caballero", "Caballero"));   //Categoría
+  $iProductos = 7; //Indica el No. de Productos
+
+  $BDAlmacen = array( 
+    array(1, 2, 3, 4, 5), //El IDProducto
+    array(0,10, 5,15, 3)); //Las Existencias
+
+  $iAlmacen = 2; //El No. de existencias
+  $BDVentas = array(
+    array(2,3), //El IDProducto
+    array(2,1), //La Cantidad
+    array(700, 200)); //El Monto
+  $iVentas = 2; //El No. de ventas al momento
+?>
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="Software Punto de Venta" content="">
-    <meta name="Gerardo Aguilar" content="">
+    <meta name="Luciano Bustamante" content="">
     <title>Inicio | E-Shopper</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
     <link href="css/font-awesome.min.css" rel="stylesheet">
@@ -32,8 +52,8 @@
 					<div class="col-sm-6">
 						<div class="contactinfo">
 							<ul class="nav nav-pills">
-								<li><a href="#"><i class="fa fa-phone"></i> +52 618 1200 776</a></li>
-								<li><a href="#"><i class="fa fa-envelope"></i> atencionclientes@gmail.com</a></li>
+								<li><a href="#"><i class="fa fa-phone"></i> +52 618 120 56 89</a></li>
+								<li><a href="#"><i class="fa fa-envelope"></i> luciano bustamante@gmail.com</a></li>
 							</ul>
 						</div>
 					</div>
@@ -52,7 +72,9 @@
 							<a href="index.php"><img src="images/home/logo.png" alt="" /></a>
 						</div>						
 					</div>
+					<H3>SOFTWARE PUNTO DE VENTA </H3>
 					<div class="col-sm-8">
+						Por: LUCIANO BUSTAMANTE y PEÑA SARACHO
 						<div class="shop-menu pull-right">
 							<ul class="nav navbar-nav">
 								<li><a href="checkout.php"><i class="fa fa-crosshairs"></i> Pagar</a></li>
@@ -147,7 +169,8 @@
 						<div class="panel-group category-products" id="accordian"><!--category-productsr-->
 							<div class="panel panel-default">
 								<div class="panel-heading">
-									<h4 class="panel-title"><a href="#">Categoría 1</a></h4>
+									<h4 class="panel-title"><a href="#">Damas</a></h4>
+									<h4 class="panel-title"><a href="#">Caballeros</a></h4>
 								</div>
 							</div>
 						</div><!--/category-products-->
@@ -162,26 +185,32 @@
 				<div class="col-sm-9 padding-right">
 					<div class="features_items"><!--features_items-->
 						<h2 class="title text-center">Catálogo de Productos</h2>
+					     	<?php
+                                for ($i=0; $i < $iProductos;$i++){ 
+                             ?>
 						<div class="col-sm-4">
 							<div class="product-image-wrapper">
 								<div class="single-products">
 										<div class="productinfo text-center">
-											<img src="images/home/product1.jpg" alt="" />
-											<h2>$56</h2>
+								           <?php
+								           $img = $BDProductos[0][$i];
+								           ?>
+											<img src="images/home/<?php echo $img; ?>.jpg" width = "210" height = "180" alt="" />
+											<h2><?php echo $BDProductos [2][$i]; ?> </h2>
 											<p>Producto 1</p>
 											<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
 										</div>
 										<div class="product-overlay">
 											<div class="overlay-content">
-												<h2>$56</h2>
-												<p>Producto 1</p>
+												<h2><?php echo $BDProductos [2][$i]; ?> </h2>
+												<p><?php echo $BDProductos [1][$i]; ?></p>
 												<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
 											</div>
 										</div>
 								</div>								
 							</div>
 						</div>
-						
+						<?php } ?>
 					</div><!--features_items-->
 					
 					
@@ -192,6 +221,19 @@
 						<div id="recommended-item-carousel" class="carousel slide" data-ride="carousel">
 							<div class="carousel-inner">
 								<div class="item active">	
+									<div class="col-sm-4">
+										<div class="product-image-wrapper">
+											<div class="single-products">
+												<div class="productinfo text-center">
+													<img src="images/home/recommend1.jpg" alt="" />
+													<h2>$50</h2>
+													<p>Producto Esp 1</p>
+													<a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>Agregar al Carrito</a>
+												</div>
+												
+											</div>
+										</div>
+									</div>
 									<div class="col-sm-4">
 										<div class="product-image-wrapper">
 											<div class="single-products">
